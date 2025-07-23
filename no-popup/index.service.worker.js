82e3fceaa65e0d878201b762548dd3c16a651483
Jiwoo Hwang <jw000922@gmail.com>
2025-07-23 10:43:23 +0200

    Popup and no-popup, enabled virtual keyboard

--- no-popup/index.service.worker.js
+++ no-popup/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1753259405|6639314774";
+const CACHE_VERSION = "1753260129|7363415345";
 const CACHE_PREFIX = "Chempion-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
--- popup/index.service.worker.js
+++ popup/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1753259375|6609811618";
+const CACHE_VERSION = "1753260146|7380576681";
 const CACHE_PREFIX = "Chempion-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
