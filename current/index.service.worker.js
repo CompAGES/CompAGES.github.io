f83af8b1b68c0ecae3a24052ec823fbab0e9acd1
Jiwoo Hwang <jw000922@gmail.com>
2026-05-23 12:59:03 +0200

    experiment 1

--- current/index.service.worker.js
+++ current/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1779438096|197257632535";
+const CACHE_VERSION = "1779533841|293002306124";
 const CACHE_PREFIX = "Chempion-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
