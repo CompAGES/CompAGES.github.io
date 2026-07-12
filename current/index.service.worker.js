6a6e3cf6fc60ba66ef438f91447d7dbf7af252dc
Jiwoo Hwang <jw000922@gmail.com>
2026-07-12 22:43:59 +0200

    fixed import bug

--- current/index.service.worker.js
+++ current/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1782508475|37668549711";
+const CACHE_VERSION = "1783888932|21194347375";
 const CACHE_PREFIX = "Chempion-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
