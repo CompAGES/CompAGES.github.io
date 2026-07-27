a29049fe3a2252e805901d03c24ac77c54318453
Jiwoo Hwang <jw000922@gmail.com>
2026-07-27 12:12:31 +0200

    fixed action mode

--- color/index.service.worker.js
+++ color/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1785146430|394044660";
+const CACHE_VERSION = "1785147092|53288162";
 const CACHE_PREFIX = "Chempion-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
