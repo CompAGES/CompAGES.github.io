2c840bd9fe20a87a299ce1bbc1b15f1f6ea64297
Jiwoo Hwang <jw000922@gmail.com>
2026-07-28 19:49:54 +0200

    summary button visibility

--- color/index.service.worker.js
+++ color/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1785228917|81878340407";
+const CACHE_VERSION = "1785260915|113876203544";
 const CACHE_PREFIX = "Chempion-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
