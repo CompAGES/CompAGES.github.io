df56ec0ae85e86594e73bca38c2d3b5367bb652a
Jiwoo Hwang <jw000922@gmail.com>
2026-09-15 20:26:22 +0200

    text changes

--- color_without_cvs/index.service.worker.js
+++ color_without_cvs/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1785260876|113837704739";
+const CACHE_VERSION = "1789496707|1194362612";
 const CACHE_PREFIX = "Chempion-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
